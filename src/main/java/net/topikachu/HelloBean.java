aa144d5813e8bf9d23d3d8bacba8e55b784993c8
GongYi <topikachu@163.com>
2012-08-04 10:06:32 +0800

    OPEN - task 2: A working jsf2 hello world https://github.com/topikachu/sparrow-runtime/issues/issue/2

--- src/main/java/net/topikachu/HelloBean.java
+++ src/main/java/net/topikachu/HelloBean.java
@@ -1,10 +1,18 @@
 package net.topikachu;
 
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
 @Named
+@RequestScoped
 public class HelloBean {
+	private String name;
+	
 	public String getName(){
-		return "World";
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 }

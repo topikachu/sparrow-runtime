48138fb2ce5fae94e3255f900a8ebf93cf0c7d57
GongYi <topikachu@163.com>
2012-08-05 15:40:01 +0800

    OPEN - task 9: integrate with workflow https://github.com/topikachu/sparrow-runtime/issues/issue/9

--- src/main/java/net/topikachu/HelloBean.java
+++ src/main/java/net/topikachu/HelloBean.java
@@ -3,8 +3,11 @@ package net.topikachu;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 import net.topikachu.sparrow.datamodel.Asset;
+import net.topikachu.sparrow.ejb.JpaManager;
 
 @Named
 @RequestScoped
@@ -12,11 +15,14 @@ public class HelloBean {
 	private String name;
 	
 	
-	@Inject 
-	public String getName(){
+	@Inject JpaManager jpaManager;
+	public String getName() throws NamingException{
+		
+		String dataSourceJndiName="java:app/jdbc/sparrow";
+		Object o=new InitialContext().lookup(dataSourceJndiName);
 		Asset asset=new Asset();
 		asset.setAssetTag("hello");
-		
+		jpaManager.saveEntity(asset);
 		
 		return asset.getId();
 		

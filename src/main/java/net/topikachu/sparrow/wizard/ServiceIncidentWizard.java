9d40d0ec1c9b66c1cec63c191ab2fed549551ad8
GongYi <topikachu@163.com>
2012-08-05 13:29:26 +0800

    OPEN - task 8: Integrate conversation with generic wizard https://github.com/topikachu/sparrow-runtime/issues/issue/8

--- src/main/java/net/topikachu/sparrow/wizard/ServiceIncidentWizard.java
+++ src/main/java/net/topikachu/sparrow/wizard/ServiceIncidentWizard.java
@@ -2,13 +2,28 @@ package net.topikachu.sparrow.wizard;
 
 import java.io.Serializable;
 
+import javax.ejb.Stateful;
+import javax.enterprise.context.Conversation;
 import javax.enterprise.context.ConversationScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 @Named
 @ConversationScoped
 public class ServiceIncidentWizard implements Serializable {
+	@Inject Conversation conversation;
+	public void start(){
+		if (conversation.isTransient()){
+		conversation.begin();
+		}
+	}
+	
 	public String next(){
-		return "Detail.xhmtl";
+		return "Detail.xhmtl?faces-redirect=true";
+	}
+	
+	public String finish(){
+		conversation.end();
+		return "Confirm.xhtml?faces-redirect=true";
 	}
 }
